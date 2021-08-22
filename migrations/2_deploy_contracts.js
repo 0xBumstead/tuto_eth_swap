f4e027ce43f3a4cc415af5b66ef37e7628a90aee
fmcch <fmhcry@gmail.com>
2021-08-22 15:50:50 +0200

    1. Smoke Test

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const DevToken = artifacts.require("DevToken");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(DevToken);
+  const token = await DevToken.deployed();
+
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  //Transfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000');
+};
---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "./DevToken.sol";
+
+contract EthSwap {
+	string public name = "EthSwap Instant Exchange";
+}
