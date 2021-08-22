ada4913aaca4a761e54948ee399e2181d6896bda
fmcch <fmhcry@gmail.com>
2021-08-23 00:07:57 +0200

    2. Contract (buy)

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -4,4 +4,28 @@ import "./DevToken.sol";
 
 contract EthSwap {
 	string public name = "EthSwap Instant Exchange";
+	DevToken public token;
+	uint public rate = 200;
+
+	event TokenPurchased(
+		address account,
+		address token,
+		uint amount,
+		uint rate
+	);
+
+	constructor(DevToken _token) public {
+		token = _token;
+	}
+
+	function buyTokens () public payable {
+		uint tokenAmount = msg.value * rate;
+
+		require(token.balanceOf(address(this)) >= tokenAmount);
+
+		token.transfer(msg.sender, tokenAmount);
+		
+		emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+	}
+	
 }
