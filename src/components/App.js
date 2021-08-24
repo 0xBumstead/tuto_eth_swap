87b67938ec0af95f8f55a5172575b7ab51a5e121
fmcch <fmhcry@gmail.com>
2021-08-24 16:02:55 +0200

    4. Website setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+import Navbar from './Navbar';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount () {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      ethBalance: '0'
+    };
+  } 
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://github.com/fmcch"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Bumstead’s dev
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="https://github.com/fmcch"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Bumstead’s dev - Work in progress</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://github.com/fmcch"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  My github
-                </a>
+               
               </div>
             </main>
           </div>
