ca6187477b34fa78942ba5ba805cf61f708b1e8c
fmcch <fmhcry@gmail.com>
2021-08-27 18:44:59 +0200

    Updates

---
+++ precache-manifest.894c9d90198d50ec7af21c35dc9b333d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8193828a7190cd332dd6171471048095",
+    "url": "/tuto_eth_swap/static/media/token-logo.8193828a.png"
+  },
+  {
+    "revision": "3829b744cdb20d450cf4",
+    "url": "/tuto_eth_swap/static/js/runtime~main.3829b744.js"
+  },
+  {
+    "revision": "14c596354319b903bcae",
+    "url": "/tuto_eth_swap/static/js/main.14c59635.chunk.js"
+  },
+  {
+    "revision": "164da741037676e938c2",
+    "url": "/tuto_eth_swap/static/js/1.164da741.chunk.js"
+  },
+  {
+    "revision": "14c596354319b903bcae",
+    "url": "/tuto_eth_swap/static/css/main.200f5f21.chunk.css"
+  },
+  {
+    "revision": "164da741037676e938c2",
+    "url": "/tuto_eth_swap/static/css/1.f64a7861.chunk.css"
+  },
+  {
+    "revision": "b5c8b2b7e1ed2fd190fd7ae11d77d0a1",
+    "url": "/tuto_eth_swap/index.html"
+  }
+];
